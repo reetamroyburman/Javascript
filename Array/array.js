877f478375e351cd92918a6fc535e6208f91e7f8
Reetam Roy Burman <contact.iamreetam@gmail.com>
2023-09-16 19:16:26 +0530

    Array Methods

---
+++ Array/array.js
@@ -0,0 +1,6 @@
+
+// JavaScript Array length
+// The length property returns the length (size) of an array:
+const variable = ["var1", "var2", "var3", "var4"];
+let size = variable.length;
+console.log(size);
